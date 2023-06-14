3976c6587635450778a7d51a46adea6f16f5ff2a
Krishna Eduardo <krishnaghabril@gmail.com>
2023-06-14 11:05:04 -0300

    Update

---
+++ Aula10_ListaDuplamenteEncadeada/main.py
@@ -0,0 +1,12 @@
+from Lista import Lista
+
+lista = Lista()
+
+
+lista.addNoInicio(5)
+lista.addNoInicio(2)
+lista.addNoFim(8)
+lista.addNoFim(8)
+
+lista.removerDoInicio()
+lista.removerDoFim()
---
+++ Aula11_FIFO/main.py
@@ -0,0 +1,10 @@
+from Fila import Fila 
+
+fila = Fila()
+
+fila.imprimir()
+
+fila.add("João")
+fila.add("Maria")
+fila.add("José")
+fila.add("Julia")
